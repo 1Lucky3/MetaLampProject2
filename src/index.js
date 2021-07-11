717eb8b9951bafbe8a7606f3544d771cd5966d1a
1Lucky3 <82760431+1Lucky3@users.noreply.github.com>
2021-07-11 15:21:32 +0500

    added default slider functional on JavaScript's

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import './style.scss'
 import './slider.ts'
 
 const sliderBtn = document.querySelector(".slider__btn");
-sliderBtn.onmousedown = function(event){
-  moveAt(event.pageX);
-  function moveAt(pageX) {
-    sliderBtn.style.left = pageX + 'px';
-  }
-  function onMouseMove(event) {
-    moveAt(event.pageX);
-  }
-  document.addEventListener('mousemove', onMouseMove);
+const sliderBar = document.querySelector(".slider__bar");
+const slider = document.querySelector(".slider");
 
-  sliderBtn.onmouseup = function() {
+sliderBtn.onmousedown = function(event){
+  event.preventDefault();
+  let sliderPos = sliderBar.getBoundingClientRect();
+  let btnPos = sliderBtn.getBoundingClientRect();
+  let btnShift = event.clientX - btnPos.left;
+  
+  function onMouseMove(event) {
+    let leftShift = event.clientX - sliderBar.getBoundingClientRect().left - btnShift;
+    let leftShiftMax = sliderPos.width - btnPos.width;
+    if(leftShift<0){
+      leftShift = 0;
+    }
+    if(leftShift>leftShiftMax){
+      leftShift = leftShiftMax;
+    }
+    sliderBtn.style.left = leftShift + "px";
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  function onMouseUp (){
     document.removeEventListener('mousemove', onMouseMove);
-    sliderBtn.onmouseup = null;
-  };
+    document.removeEventListener("mouseup", onMouseUp);
+  }
+
   sliderBtn.ondragstart = function() {
     return false;
   };
